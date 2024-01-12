4c3924eedd4aa8ecb3060ed0f794247ce1a59c68
Weirong Xu <weirongxu.raidou@gmail.com>
2024-01-12 15:14:44 +0800

    feat: @typescript-eslint/no-unnecessary-condition

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,10 @@
+// 'test' is defined but never used. (eslint @typescript-eslint/no-unused-vars)
 function test() {
+  // is assigned a value but never used. (eslint @typescript-eslint/no-unused-vars)
   const a = 1;
   const b = 2;
   const c = 3;
+  // Unexpected console statement. (eslint no-console)
   console.log('test');
   console.warn('test');
 }
