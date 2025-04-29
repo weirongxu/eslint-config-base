1e38dd5fe35a264b14f5ff43ade2b264118039f7
Weirong Xu <weirongxu.raidou@gmail.com>
2025-04-29 18:50:00 +0800

    test: no-var

--- test/test.ts
+++ test/test.ts
@@ -3,6 +3,7 @@ function test() {
   // is assigned a value but never used. (eslint @typescript-eslint/no-unused-vars)
   const a = 1;
   const b = 2;
+  // eslint-disable-next-line no-var
   var c = 3;
   // Unexpected console statement. (eslint no-console)
   console.log('test');
